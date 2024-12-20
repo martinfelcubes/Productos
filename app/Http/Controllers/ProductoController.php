8d5e6c9959947f5b6965d6883f1b0efea85a38c6
martinfelcubes <martinfelcubes@gmail.com>
2024-12-20 11:45:27 -0500

    Comentando funciones

--- app/Http/Controllers/ProductoController.php
+++ app/Http/Controllers/ProductoController.php
@@ -17,23 +17,49 @@ class ProductoController extends Controller
         $this->productRepository = $productRepository;
     }
 
- 
+  /**
+     * Muestra una lista de todos los productos.
+     *
+     * Este método recupera todos los productos de la base de datos
+     * utilizando el repositorio de productos y los pasa a la vista
+     * 'Productos.index' para su presentación.
+     *
+     * @return \Illuminate\View\View
+     * @author Martin Cubillos
+     */
+
     public function index()
     {
         $Productos = $this->productRepository->all();
-
-       // $Productos = Product::where('id','LIKE','%'.$texto.'%')->get();
-        
         return view("Productos.index", compact('Productos'));
     }
 
-   
-    public function create()
+    /**
+     * Muestra el formulario para crear un nuevo producto.
+     *
+     * Este método retorna la vista 'Productos.create' donde el usuario
+     * puede ingresar los detalles del nuevo producto.
+     *
+     * @return \Illuminate\View\View
+     *@author Martin Cubillos
+     */
+        public function create()
     {
         return view("Productos.create");
     }
 
-   
+
+
+    /**
+     * Almacena un nuevo producto en la base de datos.
+     *
+     * Este método valida los datos proporcionados en la solicitud,
+     * crea un nuevo producto y lo guarda en la base de datos.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\RedirectResponse
+     * *@author Martin Cubillos
+     */
     public function store(Request $request)
     {
 
@@ -43,36 +69,47 @@ class ProductoController extends Controller
             'stock'=>'required',
         ]);
 
-
         $producto = Product::create($request->all());
         $this->productRepository->save($producto);
-       // dd($request->all());
-       // $producto = Product::create($request->all());
         return to_route("indexproducto");
     }
 
- 
+  /**
+     * Muestra los detalles de un producto específico.
+     *
+     * Este método verifica si el ID del producto está presente en la
+     * solicitud, busca el producto en la base de datos y retorna la
+     * vista 'Productos.show' con los detalles del producto.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\View\View|\Illuminate\Http\RedirectResponse
+     * @author Martin Cubillos
+     */
     public function show(Request $request)
 {
-    // Verificar si el ID está presente en la solicitud
     if ($request->id != null) {
-        
-        // Buscar el producto usando el repositorio
         $producto = $this->productRepository->findby($request->id);
 
-        // Si el producto fue encontrado, retornamos la vista
         if ($producto) {
             return view("Productos.show", compact("producto"));
         } else {
-            // Si el producto no fue encontrado, redirigimos con un mensaje de error
             return redirect()->back()->with("error", "Producto no encontrado");
         }
-
     } else {
-        // Si no se proporciona un ID, redirigir con un mensaje de error
         return redirect()->back()->with("error", "Id no proporcionado");
     }
 }
+
+  /**
+     * Muestra el formulario para editar un producto existente.
+     *
+     * Este método busca el producto por su ID y retorna la vista
+     * 'Productos.edit' con los detalles del producto para su edición.
+     *
+     * @param  string  $id
+     * @return \Illuminate\View\View
+     * @author Martin Cubillos
+     */
 
   
     public function edit(string $id)
@@ -83,7 +120,16 @@ class ProductoController extends Controller
     }
 
     /**
-     * Update the specified resource in storage.
+     * Actualiza los detalles de un producto existente.
+     *
+     * Este método valida los datos proporcionados en la solicitud,
+     * actualiza el producto correspondiente en la base de datos y
+     * redirige a la lista de productos.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string  $id Elemento a editar de la tabla productos
+     * @return \Illuminate\Http\RedirectResponse
+     * @author Martin Cubillos
      */
     public function update(Request $request, string $id)
     {
@@ -99,9 +145,17 @@ class ProductoController extends Controller
         return to_route("indexproducto");
     }
 
-    /**
-     * Remove the specified resource from storage.
+  /**
+     * Elimina un producto de la base de datos.
+     *
+     * Este método busca el producto por su ID, lo elimina de la base
+     * de datos y redirige a la lista de productos.
+     *
+     * @param  string  $id
+     * @return \Illuminate\Http\RedirectResponse
+     * @author Martin Cubillos
      */
+    
     public function destroy(string $id)
     {
 
